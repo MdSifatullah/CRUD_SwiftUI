3e1f8ff78efd4758fb1840cda7e1bf40b6edb192
Md Sifat <cse.sifat@gmail.com>
2020-02-19 16:02:55 +0600

    List View loaded , problem to show data from realm database

--- CRUD_SwiftUI/ContentView.swift
+++ CRUD_SwiftUI/ContentView.swift
@@ -12,11 +12,13 @@ import RealmSwift
 struct ContentView: View {
     @State private var inputName = ""
     @State private var inputEmail = ""
+    @State private var isloaded = false
+    @State private var result: Results<DataSet>?
     var body: some View {
         ZStack{
             LinearGradient(gradient: .init(colors: [Color("1"), Color("2")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             VStack{
-                Text("Personal Information").font(.system(size: 22)).foregroundColor(Color("Color"))
+                Text("Login").font(.system(size: 22)).foregroundColor(Color("Color"))
                 HStack{
                     
                     Image(systemName: "person.fill").resizable().frame(width: 20, height: 20)
@@ -27,7 +29,7 @@ struct ContentView: View {
                 
                 HStack{
                     Image(systemName: "envelope").resizable().frame(width: 20, height: 20)
-                    TextField("Enter Your Email",  text: $inputEmail).padding(.leading, 12).font(.system(size: 20))
+                    TextField("Enter Your Password",  text: $inputEmail).padding(.leading, 12).font(.system(size: 20))
                 }.padding(12)
                     .background(Color("Color"))
                     .cornerRadius(25)
@@ -56,14 +58,15 @@ struct ContentView: View {
                         .cornerRadius(25)
                     Button(action: {
                         let config = Realm.Configuration(schemaVersion: 1)
-                            do{
-                                let realm = try Realm(configuration: config)
-                                let result = realm.objects(DataSet.self)
-                                print(result)
-                            }
-                            catch{
-                                print (error)
-                            }
+                        do{
+                            let realm = try Realm(configuration: config)
+                            self.result = realm.objects(DataSet.self)
+                            print(self.result![1].name)
+                        }
+                        catch{
+                            print (error)
+                        }
+                        self.isloaded.toggle()
                         
                     })
                     {
@@ -72,11 +75,29 @@ struct ContentView: View {
                         .background(LinearGradient(gradient: .init(colors: [Color("2"), Color("1")]), startPoint: .leading, endPoint: .trailing))
                         .cornerRadius(25)
                 }
+                Spacer()
+                VStack{
+                    Text("Display Data").foregroundColor(.white).padding(.leading, 12).font(.system(size: 25))
+                
+                    List (0..<5){_ in
+                        Image(systemName: "photo")
+                        VStack (alignment: .leading){
+                            Text("Name)").foregroundColor(.gray).font(.system(size: 15))
+                            Text("name@gmail.com)").foregroundColor(.green).font(.system(size: 8))
+                        }
+                    }
+                }.padding()
+                .background(Color("1"))
                 
             }.padding()
+            
         }
     }
+    
 }
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
